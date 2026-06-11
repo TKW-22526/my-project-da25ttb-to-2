c17cda0a267988674f9ba4932a6bb9a648f648d8
thanh <ngocdanthanhdt@gmail.com>
2026-06-11 17:39:24 +0700

    Update 11062026

--- js/lib.js
+++ js/lib.js
@@ -1,4 +1,41 @@
 /*Mã nguồn Javascript*/
+//Khai báo một đối tượng
+const product = {
+    id:"1",
+    name:"Hoa hồng Pháp",
+    price:250000,
+    description:"Đây là giống Hoa được yêu thích ở Pháp được nhập khẩu  về Việt Nam ...",
+    image:"../assets/images/rose.jpg",
+    link:"chi-tiet.html"
+};
+
+const products = [
+    {
+        id:"1",
+        name:"Hoa hồng Pháp",
+        price:150000,
+        description:"Đây là giống Hoa được yêu thích ở Pháp được nhập khẩu  về Việt Nam ...",
+        image:"../assets/images/rose.jpg",
+        link:"chi-tiet.html"
+    },
+    {
+        id:"2",
+        name:"Hoa hồng Việt Nam",
+        price:650000,
+        description:"Đây là giống Hoa được yêu thích ở Pháp được nhập khẩu  về Việt Nam ...",
+        image:"../assets/images/rose2.jpg",
+        link:"chi-tiet.html"
+    },
+    {
+        id:"3",
+        name:"Hoa hướng dương",
+        price:250000,
+        description:"Đây là giống Hoa được yêu thích ở Pháp được nhập khẩu  về Việt Nam ...",
+        image:"../assets/images/sunflower.jpg",
+        link:"chi-tiet.html"
+    }
+];
+
 function inBCC(n)
 {
     let result = "";
@@ -63,4 +100,34 @@ function addItem(name, price, description, link, image)
     item.appendChild(containerInfo);
     //Đưa khung chứa container-product-list vào container
     document.getElementById("container-product-list").appendChild(item);
+}
+
+function addItemV2(obj)
+{
+    const list = document.getElementById("product-list");
+    list.innerHTML += `
+        <div class="col">
+                    <div class="card product-item h-100 ">
+                        <div class = "product-image ratio ratio-1x1">
+                            <img class="card-img-top object-fit-cover" src="${obj.image}" alt="${obj.name}">
+                        </div>
+                        <div class="card-body bg-light product-info text-center">
+                            <h4 class = "card-title text-danger">${obj.name}</h4>
+                            <h5 class = "card-text">${obj.price}</h5>
+                            <p style = "text-align:justify; line-height:1.5" class = "card-text">${obj.description}</p>
+                            <a class = "btn btn-info" href = "${obj.link}?ma=${obj.id}">Xem chi tiết</a>
+                        </div>
+                    </div>
+                </div>
+    `;
+}
+
+function loadAllProducts(array)
+{
+    let i = 0;
+    while(i<array.length)
+    {
+        addItemV2(array[i]);
+            i++;
+    }
 }
